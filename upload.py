8d211f1e639892a9d99fccc455a5859936baaab8
Parand Darugar <darugar@Parand-Darugars-MacBook.local>
2011-02-20 14:28:54 -0800

    Initial commit.

---
+++ upload.py
@@ -0,0 +1,54 @@
+import os
+from google.appengine.ext import webapp
+from google.appengine.ext import db
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+class Content(db.Model):
+    username = db.StringProperty()
+    uid = db.StringProperty()
+    contents = db.BlobProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
+
+class UploadPage(webapp.RequestHandler):
+    def post(self):
+        item = Content(
+            username=self.request.get("username"),
+            uid     =self.request.get("uid"),
+            contents=db.Blob(self.request.get("file"))
+        )
+        item.put()
+        print self.request.get("file")
+        self.response.out.write("OK")
+
+class ListPage(webapp.RequestHandler):
+    def get(self):
+        items = Content.all().order("-date")
+        path = os.path.join(os.path.dirname(__file__), 'templates/list.html')
+        self.response.out.write(template.render(path, {'items':items}))
+
+class ShowPage(webapp.RequestHandler):
+    def get(self, key):
+        item = Content.get(key)
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write(item.contents)
+
+class TestPage(webapp.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+        print "path=", path
+        self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication([
+    ('/test', TestPage),
+    ('/list', ListPage),
+    ('/show/(.*)', ShowPage),
+    ('/upload', UploadPage)
+], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
