cff035133e0d9c139a9c2621f4c6f83c7858e407
Bindu Wavell <bindu@wavell.net>
2011-02-21 22:58:14 -0700

    Only specified user may list items Created config.yaml so this can be based on config but not reading yet, currently the allowed user is hard coded. Also added the ability to delete items from the list.

--- upload.py
+++ upload.py
@@ -1,4 +1,5 @@
 import os
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -24,25 +25,44 @@ class UploadPage(webapp.RequestHandler):
 
 class ListPage(webapp.RequestHandler):
     def get(self):
-        items = Content.all().order("-date")
-        path = os.path.join(os.path.dirname(__file__), 'templates/list.html')
-        self.response.out.write(template.render(path, {'items':items}))
+        user = users.get_current_user()
+        if user:
+            self.response.out.write(
+                'Hello %s <a href="%s">Sign out</a><br>' % 
+                (user.nickname(), users.create_logout_url("/list"))
+            )
+            if user.email() == 'your_account@gmail.com':
+                items = Content.all().order("-date")
+                path = os.path.join(os.path.dirname(__file__), 'templates/list.html')
+                self.response.out.write(template.render(path, {'items':items}))
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
+class DeletePage(webapp.RequestHandler):
+    def get(self, key):
+        item = Content.get(key)
+        Content.delete(item)
+        self.redirect('/list', False)
+        
 class ShowPage(webapp.RequestHandler):
     def get(self, key):
         item = Content.get(key)
-        self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.headers['Content-Type'] = 'text/plain'
+        #evaluate the extension, txt and log should be text, otherwise octet-stream
+        self.response.headers['Content-Type'] = 'application/octet-stream'
         self.response.out.write(item.contents)
 
 class TestPage(webapp.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+        template_values = ()
         self.response.out.write(template.render(path, template_values))
 
 application = webapp.WSGIApplication([
     ('/test', TestPage),
     ('/list', ListPage),
     ('/show/(.*)', ShowPage),
+    ('/delete/(.*)', DeletePage),
     ('/upload', UploadPage)
 ], debug=True)
 
